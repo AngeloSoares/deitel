b1b733e4fc063c8dd2ff9e5a4c8ec873a238bd32
Angelo Soares <soaresf43angelo@hotmail.com>
2023-11-05 21:00:57 -0300

    Capitulo 2.5 - Apredendo Java
    Subindo Addition
    
    Angelo Soares
    05NOV2023

---
+++ src/capitulo2/Addition.java
@@ -0,0 +1,24 @@
+package capitulo2;
+//2.7. Programa de adição que insere dois números, então exibe a soma deles.
+
+import java.util.Scanner;
+public class Addition {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+        int sum;
+
+        System.out.print("Digite um número inteiro: ");
+        number1 = input.nextInt();
+
+        System.out.print("Digite outro número inteiro: ");
+        number2 = input.nextInt();
+
+        sum = number1 + number2;
+
+        System.out.printf("A soma é %d%n", sum);
+    }
+}
