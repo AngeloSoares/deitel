f1047b410091df94e198ffe16c210d040fb7abda
Angelo Soares <soaresf43angelo@hotmail.com>
2023-12-23 23:07:37 -0300

    Capitulo 6 - Sobrecarga de Métodos.
    Angelo Soares
    23DEZ2023

---
+++ src/Capitulo6/MethodOverLoad.java
@@ -0,0 +1,27 @@
+package Capitulo6;
+
+//Declarações de métodos sobrecarregados ou sobrecarga de métodos.
+//São simplesmente métodos que contem o mesmo nome, mas com argumentos diferentes, sejam ordem, tipo ou em números.
+public class MethodOverLoad
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Quadrado do inteiro é %d%n", square(7));
+        System.out.printf("Quadrado do double é %.2f%n", square(7.5));
+    }
+
+
+    public static int square(int intValue)
+    {
+        System.out.printf("%nChamada square com argumento int: %d%n", intValue);
+
+        return intValue * intValue;
+    }
+
+    public static double square(double doubleValue)
+    {
+        System.out.printf("%nChamada square com argumento double: %.2f%n", doubleValue);
+
+        return doubleValue * doubleValue;
+    }
+}
---
+++ src/Capitulo6/Scope.java
@@ -0,0 +1,45 @@
+package Capitulo6;
+
+// A Classe Scope demostra os escopos de campo e variável local.
+public class Scope
+{
+
+    private static int x = 1;  // campo acessível para todos os métodos desta classe.
+
+
+
+    public static void main(String[] args)
+    {
+        int x = 5; // variável local x do método psvm sombreia o campo x.
+
+        System.out.printf("Variável x no método main é %d%n%n", x);
+
+        useLocalVariable(); // useLocalVariable tem uma variável local x.
+        useField(); //useField utiliza o campo x da classe Scope.
+        useLocalVariable(); // useLocalVariable reinicializa a variável local x.
+        useField(); //campo x da classe Scope retém seu valor.
+
+        System.out.printf("Variável x no método main é %d%n%n", x);
+    }
+
+
+    public static void useLocalVariable()
+    {
+        int x = 25; // inicializa toda vez que useLocalVariable é chamado.
+
+        System.out.printf("Variável Local x entrando no método useLocalVariable: %d%n", x);
+        ++x; //modifica a variável local x deste método.
+        System.out.printf("Variável Local x antes de sair do método useLocalVariable: %d%n%n", x);
+    }
+
+
+
+
+    public static void useField()
+    {
+        System.out.printf("Variável Local x entrando no método useField: %d%n", x);
+        x *= 10;
+        System.out.printf("Variável Local x antes de sair do método useField: %d%n%n", x);
+
+    }
+}
